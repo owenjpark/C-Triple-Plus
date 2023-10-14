46b1859d689b8b103a9ab752eff99a47c8b80798
Owen Park <owenjpark@gmail.com>
2023-10-14 15:22:24 -0700

    everything but fail cases working locally, NOTE: need to organize files

--- src/parse.cpp
+++ src/parse.cpp
@@ -103,7 +103,6 @@ AST::~AST(){
     destructorHelper(root);
 }
 
-
 AST::node* parse(vector<vecComponent> lexVec, int index, string parent){
     index++; // eat the parenthesis
     int lCounter = 1;
@@ -165,6 +164,61 @@ void printEquation(AST::node* nodeParam) { // pass head of tree to print whole t
     }
 }
 
+double evaluate(AST::node* nodeParam) {
+    double someValue = 0;
+    vector<double> childrenVal;
+    for (unsigned i = 0; i < nodeParam->children.size(); i++) {
+        childrenVal.push_back(evaluate(nodeParam->children.at(i)));
+    }
+    if (nodeParam->data == "+") {
+        for (unsigned i = 0 ; i < childrenVal.size(); i++) {
+            if (i == 0) { // first value
+                someValue = childrenVal.at(i);
+            }
+            else {
+                someValue += childrenVal.at(i);
+            }
+        }
+        return someValue;
+    }
+    else if (nodeParam->data == "-"){
+        for (unsigned i = 0 ; i < childrenVal.size(); i++) {
+            if (i == 0) { // first value
+                someValue = childrenVal.at(i);
+            }
+            else {
+                someValue -= childrenVal.at(i);
+            }
+        }
+        return someValue;
+    }
+    else if (nodeParam->data == "*"){
+        for (unsigned i = 0 ; i < childrenVal.size(); i++) {
+            if (i == 0) { // first value
+                someValue = childrenVal.at(i);
+            }
+            else {
+                someValue *= childrenVal.at(i);
+            }
+        }
+        return someValue;
+    }
+    else if (nodeParam->data == "/"){
+        for (unsigned i = 0 ; i < childrenVal.size(); i++) {
+            if (i == 0) { // first value
+                someValue = childrenVal.at(i);
+            }
+            else {
+                someValue /= childrenVal.at(i);
+            }
+        }
+        return someValue;
+    }
+    else {
+        return stod(nodeParam->data);
+    }
+}
+
 int main() {
     vector<vecComponent> someVec;
     int counter = 1;
@@ -174,7 +228,9 @@ int main() {
 
     AST::node* root = parse(someVec, 0, "");
 
-    printEquation(root);
+    // printEquation(root);
+
+    cout << evaluate(root) << endl;
 
     return 0;
 }
