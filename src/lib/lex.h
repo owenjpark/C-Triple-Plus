dbbee515b342b48c9eb2b8cd5f0658f4af18def0
Owen Park <owenjpark@gmail.com>
2023-10-14 18:39:50 -0700

    seperated lex files

--- src/lib/lex.h
+++ src/lib/lex.h
@@ -1,4 +1,3 @@
-
 #ifndef CALC_H
 #define CALC_H
 
@@ -9,12 +8,17 @@
 using namespace std;
 
 struct vecComponent {
-    string data;
-    int column;
-    int row;
+    string data = 0;
+    int column = 0;
+    int row = 0;
 
 };
 
+void lexer (const string line, const int row, vector<vecComponent> &inputVec);
+
+void printer(vector<vecComponent> someVec);
+
+void addEnd(vector<vecComponent> &inputVec, bool wasNL);
 
 
 #endif
