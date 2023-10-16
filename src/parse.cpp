b6805d05c6a033e0555c4b140b0c7b2773d102e2
Owen Park <owenjpark@gmail.com>
2023-10-16 02:37:47 -0700

    organized lexer and parser

--- src/parse.cpp
+++ src/parse.cpp
@@ -1,24 +1,26 @@
 #include "lib/parse.h"
 #include "lib/lex.h"
 
+
+
 int main() {
     vector<token> tokenVec;
+
     tokenVec = lexer();
 
     // end of lexer
 
-    AST expressionTree;
-    
     expressionChecker(tokenVec);
-    parser(tokenVec);
-    
-    // printInfix(expressionTree.root);
 
-    // double result = evaluateAST(expressionTree.root); // in case divided by 0 need to throw error without printing
+    AST someAST;
+    someAST.root = createAST(tokenVec, 0);
+    printInfix(someAST.root);
+
+    double result = evaluateAST(someAST.root); // in case divided by 0
 
     cout << endl;
 
-    // cout << result;
+    cout << result;
 
     cout << endl;
 
