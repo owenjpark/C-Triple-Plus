4550098367ec7e769211a609a1ce7f48b45fc5a9
Owen Park <owenjpark@gmail.com>
2023-10-24 21:54:07 -0700

    testing

--- src/lib/calcfunctions.cpp
+++ src/lib/calcfunctions.cpp
@@ -10,24 +10,29 @@ int findMatchingParenth(int i, vector<token> tokenVec) { // (12 + 7) should star
         if (tokenVec.at(i).type == "rParenth") {
             parenthDiff--;
         }
+        if (parenthDiff == 0) { // so it doesn't over iterate
+            break;
+        }
         i++;
     }
     if (tokenVec.at(i).type == "end") {
+        // cout << "error0" << endl;
         error someError(tokenVec.at(i).data,tokenVec.at(i).column, 2);
         throw someError;
     }
 
-    i--; // last one redundant; goes past "("
     return i;
 }
 
 void expressionChecker2(unsigned startIndex, unsigned endIndex, vector<token> tokenVec) {
     if (tokenVec.size() == 1) { // only end token
+        // cout << "error1" << endl;
         error someError(tokenVec.at(0).data, tokenVec.at(0).column, 2);
         throw someError;
     }
     // has to have at least 1 real token in it
     if (tokenVec.at(startIndex).type != "num" && tokenVec.at(startIndex).type != "var" && tokenVec.at(startIndex).type != "lParenth") { // doesn't start with big 3
+        // cout << "error2" << endl;
         error someError(tokenVec.at(startIndex).data, tokenVec.at(startIndex).column, 2);
         throw someError;
     }
@@ -40,28 +45,34 @@ void expressionChecker2(unsigned startIndex, unsigned endIndex, vector<token> to
             i = endParenthIndex;
             
             if (tokenVec.at(i + 1).type != "op" && tokenVec.at(i + 1).type != "eq" && tokenVec.at(i + 1).type != "end") {
+                // cout << "error3" << endl;
                 error someError(tokenVec.at(i + 1).data, tokenVec.at(i + 1).column, 2);
                 throw someError;
             }
         }
         if (tokenVec.at(i).type == "num" || tokenVec.at(i).type == "var") {
             if (tokenVec.at(i + 1).type != "op" && tokenVec.at(i + 1).type != "eq" && tokenVec.at(i + 1).type != "end") {
+                // cout << "error4" << endl;
                 error someError(tokenVec.at(i + 1).data, tokenVec.at(i + 1).column, 2);
                 throw someError;
             }
         }
         if (tokenVec.at(i).type == "op") {
+            cout << "yes\n";
             if (tokenVec.at(i + 1).type != "num" && tokenVec.at(i + 1).type != "var" && tokenVec.at(i + 1).type != "lParenth") {
+                // cout << "error5" << endl;
                 error someError(tokenVec.at(i + 1).data, tokenVec.at(i + 1).column, 2);
                 throw someError;
             }
         }
         if (tokenVec.at(i).type == "eq") {
             if (tokenVec.at(i - 1).type != "var") {
+                // cout << "error6" << endl;
                 error someError(tokenVec.at(i).data, tokenVec.at(i).column, 2);
                 throw someError;
             }
             if (tokenVec.at(i + 1).type != "num" && tokenVec.at(i + 1).type != "var" && tokenVec.at(i + 1).type != "lParenth") {
+                // cout << "error7" << endl;
                 error someError(tokenVec.at(i + 1).data, tokenVec.at(i + 1).column, 2);
                 throw someError;
             }
