1c51517fc07ea5e7eff42646b329986051b7e43d
Owen Park <owenjpark@gmail.com>
2023-10-24 20:42:58 -0700

    first commit

--- src/lib/calc.h
+++ src/lib/calc.h
@@ -3,38 +3,18 @@
 #include <string>
 #include <vector>
 
-class AST2 {
-
-    public:
-
-    struct Node {
-        string data;
-        string type;
-        
-        Node* leftChild;
-        Node* rightChild;
-    };
-
-    AST2();
-    ~AST2();
-    Node* root;
-
-    void clear(Node* n);
-};
-
 struct error {
+    error(string data, int column, int exitCode) {
+        this->data = data;
+        this->column = column;
+        this->exitCode = exitCode;
+    }
+
     string data;
     int column;
-    int code;
+    int exitCode;
 }; 
 
-    AST2::Node* build(AST2::Node* root, vector<token> vec);
-    string stringAST2(AST2::Node* root, string equation = "");
-    float evaluate(AST2::Node* root, float result=0);
-
-    //void check(vector<token> vec);
-
-    // helper functions 
-    int precedence(vector<token> vec);
+void expressionChecker2(unsigned startIndex, unsigned endIndex, vector<token> tokenVec);
 
     
---
+++ src/lib/calcfunctions.cpp
@@ -0,0 +1,70 @@
+#include "calc.h"
+#include "lex.h"
+
+void expressionChecker2(unsigned startIndex, unsigned endIndex, vector<token> tokenVec) {
+    if (tokenVec.size() == 1) { // only end token
+        error someError(tokenVec.at(0).data, tokenVec.at(0).column, 2);
+        throw someError;
+    }
+    if (tokenVec.at(0).type != "num" && tokenVec.at(0).type != "var" && tokenVec.at(0).type != "lParenth") { // doesn't start with big 3
+        error someError(tokenVec.at(0).data, tokenVec.at(0).column, 2);
+        throw someError;
+    }
+    // at least 1 element
+    for (unsigned i = startIndex; i < endIndex; i++) { // doesn't hit end token
+        if (tokenVec.at(i).type == "lParenth") {
+            i++; // skip "("
+            int endParenthIndex = findMatchingParenth(i, tokenVec);
+            expressionChecker2(i, endParenthIndex, tokenVec);
+            i = endParenthIndex;
+            
+            if (tokenVec.at(i + 1).type != "op" && tokenVec.at(i + 1).type != "eq") {
+                error someError(tokenVec.at(i + 1).data, tokenVec.at(i + 1).column, 2);
+                throw someError;
+            }
+        }
+        if (tokenVec.at(i).type == "num" || tokenVec.at(i).type == "var") {
+            if (tokenVec.at(i + 1).type != "op" && tokenVec.at(i + 1).type != "eq") {
+                error someError(tokenVec.at(i + 1).data, tokenVec.at(i + 1).column, 2);
+                throw someError;
+            }
+        }
+        if (tokenVec.at(i).type == "op") {
+            if (tokenVec.at(i + 1).type != "num" && tokenVec.at(i + 1).type != "var" && tokenVec.at(i + 1).type != "lParenth") {
+                error someError(tokenVec.at(i + 1).data, tokenVec.at(i + 1).column, 2);
+                throw someError;
+            }
+        }
+        if (tokenVec.at(i).type == "eq") {
+            if (tokenVec.at(i - 1).type != "var") {
+                error someError(tokenVec.at(i - 1).data, tokenVec.at(i - 1).column, 2);
+                throw someError;
+            }
+            if (tokenVec.at(i + 1).type != "num" && tokenVec.at(i + 1).type != "var" && tokenVec.at(i + 1).type != "lParenth") {
+                error someError(tokenVec.at(i + 1).data, tokenVec.at(i + 1).column, 2);
+                throw someError;
+            }
+        }
+    }
+}
+
+int findMatchingParenth(int i, vector<token> tokenVec) { // (12 + 7) should start at 12, returns index of )
+    int parenthDiff = 1;
+    while (parenthDiff != 0 && !(tokenVec.at(i).type == "end")) {
+        if (tokenVec.at(i).type == "lParenth") {
+            parenthDiff++;
+        }
+        if (tokenVec.at(i).type == "rParenth") {
+            parenthDiff--;
+        }
+        i++;
+    }
+    i--;
+
+    if (tokenVec.at(i).type == "end") {
+        // TODO invalid parenthesis
+    }
+    else {
+        return i;
+    }
+}
