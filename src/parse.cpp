69117dcddf571937f45328401420d0aa04606717
Owen Park <owenjpark@gmail.com>
2023-10-25 18:01:07 -0700

    cleaning up parse

--- src/parse.cpp
+++ src/parse.cpp
@@ -6,14 +6,8 @@
 int main() {
     vector<token> tokenVec;
 
-    try {
-        tokenVec = lexer();
-    }
-    catch (error someError){
-        cout << "Syntax error on line " << someError.row << " column " << someError.column << "." << endl;
-        exit(someError.code);
-    }    
-    
+    tokenVec = lexer();
+
     parser(tokenVec);
 
     return 0;
