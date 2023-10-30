ef0fe41799a41063c298a70db4fed10b7ff54a72
Owen Park <owenjpark@gmail.com>
2023-10-29 21:55:27 -0700

    added ops to build

--- src/calc.cpp
+++ src/calc.cpp
@@ -34,20 +34,20 @@ int main() {
         printInfix2(tree.root);
         cout << endl;
 
-        try { // evaluate answer
-            double result =  evaluate(tree.root, temp, 0);
-            cout << result << endl;
-        }
-        catch (error runtime){
-            if (runtime.code == 3) {
-                cout << "Runtime error: unknown identifier " << runtime.data << endl;
-            }
-            else if (runtime.code == 0) {
-                cout << "Runtime error: division by zero."  << endl;
-            }
-            continue;
-        }
-        variables = temp; // no runtime errors, set variables to temp
+        // try { // evaluate answer
+        //     double result =  evaluate(tree.root, temp, 0);
+        //     cout << result << endl;
+        // }
+        // catch (error runtime){
+        //     if (runtime.code == 3) {
+        //         cout << "Runtime error: unknown identifier " << runtime.data << endl;
+        //     }
+        //     else if (runtime.code == 0) {
+        //         cout << "Runtime error: division by zero."  << endl;
+        //     }
+        //     continue;
+        // }
+        // variables = temp; // no runtime errors, set variables to temp
     }
    
     return 0;
