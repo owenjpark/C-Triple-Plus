685988a2d194fe60766a17ae81426b3f98c6e0a0
Owen Park <owenjpark@gmail.com>
2023-10-29 21:44:49 -0700

    added ops to build

--- src/calc.cpp
+++ src/calc.cpp
@@ -3,7 +3,6 @@
 
 int main() {
     string line;
-    vector<variable> variables;
 
     while(std::getline(std::cin, line)) {
         vector<token> tokenVec;
@@ -19,7 +18,6 @@ int main() {
         addEndToken(tokenVec, 1, line.size() + 1);
 
         AST2 tree;
-        vector<variable> temp = variables; // copy of variables vector in case of "no update on error"
         token someToken;
         try { // build tree
             tree.root = build(tokenVec, someToken);
@@ -33,21 +31,24 @@ int main() {
         
         printInfix2(tree.root);
         cout << endl;
+        
+        // vector<variable> variables;
+        // vector<variable> temp = variables; // copy of variables vector in case of "no update on error"
 
-        try { // evaluate answer
-            double result =  evaluate(tree.root, temp, 0);
-            cout << result << endl;
-        }
-        catch (error runtime){
-            if (runtime.code == 3) {
-                cout << "Runtime error: unknown identifier " << runtime.data << endl;
-            }
-            else if (runtime.code == 0) {
-                cout << "Runtime error: division by zero."  << endl;
-            }
-            continue;
-        }
-        variables = temp; // no runtime errors, set variables to temp
+        // try { // evaluate answer
+        //     double result =  evaluate(tree.root, temp, 0);
+        //     cout << result << endl;
+        // }
+        // catch (error runtime){
+        //     if (runtime.code == 3) {
+        //         cout << "Runtime error: unknown identifier " << runtime.data << endl;
+        //     }
+        //     else if (runtime.code == 0) {
+        //         cout << "Runtime error: division by zero."  << endl;
+        //     }
+        //     continue;
+        // }
+        // variables = temp; // no runtime errors, set variables to temp
     }
    
     return 0;
