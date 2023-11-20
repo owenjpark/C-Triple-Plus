086bfab09ce2bcc0714be75c2d0db0b09919163c
morganparslow <morgan.parslow@gmail.com>
2023-11-20 11:40:01 -0800

    debugging parse

--- src/lib/calcfunctions.cpp
+++ src/lib/calcfunctions.cpp
@@ -263,7 +263,7 @@ shared_ptr<AST2::Node> build(vector<token> vec, token parentToken) {
     for (unsigned i = lowestPrecedenceI + 1; i < vec.size(); i++) {
         rightVec.push_back(vec[i]);
         cout << vec[i].data;
-        if (i == lowestPrecedenceI + 1) cout << vec[i].type;
+        //if (i == lowestPrecedenceI + 1) cout << vec[i].type;
     }
     if (rightVec.size() == 0) {
         token errorToken = parentToken;
